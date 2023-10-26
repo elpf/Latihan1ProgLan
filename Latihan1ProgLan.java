88f89c7e40239b9a835018b9b41752e803731be1
elpf <elgafarma@gmail.com>
2023-10-26 10:08:44 +0700

    Menambahkan Latihan1ProgLan.java

---
+++ Latihan1ProgLan.java
@@ -0,0 +1,7 @@
+public class Latihan1ProgLan {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+        System.out.println("Ini adalah Latihan 1 Pemrograman Lanjut");
+    }
+
+}
